6d1bb5268bdc8a07d8a23125692ea08755fbdd0d
Ziwei <zhuziwei@tamu.edu>
2018-03-28 10:41:26 -0500

    update NHibernate models

---
+++ WindowsFormsApp1/WindowsFormsApp1/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateDemo
+{
+    public class Record
+    {
+        public virtual int recordID { get; set; }
+
+        public virtual int staffID { get; set; }
+
+        public virtual int itemID { get; set; }
+
+        public virtual DateTime recordDate { get; set; }
+
+        public virtual bool isReturn { get; set; }
+    }
+}
